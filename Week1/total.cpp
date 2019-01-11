bbb0b303b5e1e3079a8c08ff68915a5a1e619a30
Brian Eddy <beddy@uwf.edu>
2019-01-11 10:53:26 -0600

    first week example

---
+++ Week1/total.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int maxsize = 5;
+	int values[maxsize] = {2, 4, 3, 1, 7};
+	int total = 0;
+	for(int i = 0; i < maxsize; ++i)
+	{
+		total += values[i];
+	}
+	std::cout << total << std::endl;
+	return 0;
+}
