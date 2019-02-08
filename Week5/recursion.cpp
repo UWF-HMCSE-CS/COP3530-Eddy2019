a59b67929b1ff29e11f75486b665b30f7de18ac3
Brian Eddy <beddy@uwf.edu>
2019-02-08 10:52:01 -0600

    adding binary search

--- Week5/recursion.cpp
+++ Week5/recursion.cpp
@@ -17,22 +17,58 @@ int fib(int n)
 	return fib(n - 1) + fib(n - 2);
 }
 
+int cumulativeSum(int n)
+{
+	if(n == 1)
+		return 1;
+	return n + cumulativeSum(n - 1);
+}
+
+int GCD(int a, int b)
+{
+	if(a == b)
+		return a;
+	if(a < b)
+		return GCD(a, b - a);
+	return GCD(a - b, b);
+}
+
+int binarySearch(int array[], int low, int high, int searchElement)
+{
+	if(high < low)
+	{
+		return -1;
+	}
+
+	int mid = (low + high) / 2;
+	if(array[mid] == searchElement)
+	{
+		return mid;
+	}
+
+	if(array[mid] < searchElement)
+	{
+		return binarySearch(array, mid + 1, high,searchElement);
+	}
+
+	return binarySearch(array, low, mid - 1, searchElement);
+}
+
 int main()
 {
 	std::cout << factorial(5) << std::endl;
 	std::cout << fib(2) << std::endl;
 	std::cout << fib(3) << std::endl;
 	std::cout << fib(4) << std::endl;
-	std::cout << fib(10) << std::endl;
-	std::cout << fib(20) << std::endl;
-	std::cout << fib(30) << std::endl;
-	std::cout << fib(40) << std::endl;
-	std::cout << fib(50) << std::endl;
-	std::cout << fib(75) << std::endl;
-	std::cout << fib(100) << std::endl;
-	std::cout << fib(1000) << std::endl;
-	std::cout << fib(10000) << std::endl;
-	std::cout << fib(100000) << std::endl;
+	std::cout << "Cumulative Sum: " << cumulativeSum(2) << std::endl;
+	std::cout << "Cumulative Sum: " << cumulativeSum(3) << std::endl;
+	std::cout << "Cumulative Sum: " << cumulativeSum(4) << std::endl;
+	std::cout << "GCD: " << GCD(64,16) << std::endl;
+	std::cout << "GCD: " << GCD(8,12) << std::endl;
+
+	int array[5] = {1, 2, 3, 4, 5};
+	std::cout << "Location: " << binarySearch(array, 0, 4, 2) << std::endl;
+	std::cout << "Location: " << binarySearch(array, 0, 4, 5) << std::endl;
 	return 0;
 }
 
