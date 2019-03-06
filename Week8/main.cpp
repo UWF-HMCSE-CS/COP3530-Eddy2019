a5bb4eded9018c121012511d93d4739640deb2a2
Brian Eddy <beddy@uwf.edu>
2019-03-06 10:52:52 -0600

    showed a refactoring of insertion sort for clarity

--- Week8/main.cpp
+++ Week8/main.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 
+void swap(int* firstElement, int* secondElement)
+{
+	int temp = *firstElement;
+	*firstElement = *secondElement;
+	*secondElement = temp;
+}
+
+void moveIntoSortedPosition(int* arrayBeingSorted, int nextToSort)
+{
+	int precedingElement = nextToSort - 1;
+	while((nextToSort > 0) && (arrayBeingSorted[nextToSort] < arrayBeingSorted[precedingElement]))
+	{
+		swap(&arrayBeingSorted[nextToSort], &arrayBeingSorted[precedingElement]);
+		nextToSort = nextToSort - 1;
+		precedingElement = precedingElement - 1;
+	}
+}
+
 void insertionSort(int* arrayToSort, int size)
 {
-	int j = 1;
-	while((j > 0) && (arrayToSort[j] < arrayToSort[j-1]))
+	for(int nextToSort = 1; nextToSort < size; ++nextToSort)
 	{
-		int temp = arrayToSort[j];
-		arrayToSort[j] = arrayToSort[j-1];
-		arrayToSort[j-1] = temp;
+		moveIntoSortedPosition(arrayToSort, nextToSort);
 	}
-			
 }
 
 int main()
 {
-	int unsorted[] = {2, 1};
-	int size = 2;
+	int unsorted[] = {2, 1, 3, 9, 10, 5, 4};
+	int size = 7;
 	insertionSort(unsorted, size);
 	for(int i = 0; i < size; ++i)
 		std::cout << unsorted[i] << std::endl;
