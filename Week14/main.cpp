15bc41ff3a1acce839984012b9afac7effe02dcc
Brian Eddy <beddy@uwf.edu>
2019-04-22 10:50:35 -0500

    currently unworking dfs

--- Week14/main.cpp
+++ Week14/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include <queue>
+#include <stack>
+
+void depthFirstSearch(int graph[6][6], int start)
+{
+	std::stack<int> path;
+	int visited[6] = {0, 0, 0, 0, 0, 0};
+	visited[start] = 1;
+	path.push(start);
+	int currentIndex = start;
+	for(int vertex = 0; vertex < 6; ++vertex)
+	{
+		if((graph[currentIndex][vertex] == 1) && (visited[vertex] == 0))
+		{
+			std::cout << vertex << std::endl;
+			path.push(vertex);
+			visited[vertex] = 1;
+			currentIndex = vertex;
+		}
+	}
+}
 
 void breadthFirstSearch(int graph[6][6], int start)
 {
@@ -31,7 +51,8 @@ int main()
 			   {1,0,0,0,0,0}, 
 			   {0,1,0,0,0,1}, 
 			   {0,1,1,0,1,0}};
-	breadthFirstSearch(graph, 0);
+	//breadthFirstSearch(graph, 0);
+	depthFirstSearch(graph, 0);
 
 
 	return 0;
